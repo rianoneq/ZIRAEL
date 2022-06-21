3b466d443f8ff588cc560c7363617a548668f212
itRianone <timokhindimitry@gmail.com>
2022-06-21 22:34:56 +0300

    little fix & control card name length

--- catalog/static/js/script.js
+++ catalog/static/js/script.js
@@ -2,6 +2,7 @@ let card_description = document.querySelectorAll('.product_descr')
 let card_name = document.querySelectorAll('.product_name')
 let images = document.querySelectorAll('img')
 
+//control lenght description of product in catalog
 card_description.forEach(text => {
   let max_length = 0
   if (window.innerWidth < 320) {
@@ -20,20 +21,55 @@ card_description.forEach(text => {
     max_length = 50
   }
   else if (window.innerWidth < 1400) {
-    max_length = 85
+    max_length = 80
   }
   else if (window.innerWidth < 1800) {
-    max_length = 100
+    max_length = 90
   }
   else if (window.innerWidth > 1800) {
-    max_length = 125
+    max_length = 115
   }
   
-  //console.log(max_length)
-  if (text.innerHTML.length > max_length) {
+  if (text.innerHTML.trim().length > max_length) {
+    let cutten_text = text.innerHTML.trim().slice(0,[max_length]);
+    //console.log(cutten_text)
+    text.innerHTML = cutten_text + '...';
+  }
+});
+
+//control lenght name of product in catalog
+card_name.forEach(text => {
+  let max_length = 0
+  if (window.innerWidth < 550) {
+    max_length = 10
+  }
+  else if (window.innerWidth < 600) {
+    max_length = 10
+  }
+  else if (window.innerWidth < 1000) {
+    max_length = 11
+  }
+  else if (window.innerWidth < 1000) {
+    max_length = 12
+  }
+  else if (window.innerWidth < 1200) {
+    max_length = 14
+  }
+  else if (window.innerWidth < 1400) {
+    max_length = 18
+  }
+  else if (window.innerWidth < 1800) {
+    max_length = 20
+  }
+  else if (window.innerWidth > 1800) {
+    max_length = 25
+  }
+  
+  console.log(max_length, text.innerHTML.length, text.innerHTML)
+  if (text.innerHTML.trim().length > max_length) {
     let cutten_text = text.innerHTML.trim().slice(0,[max_length]);
     console.log(cutten_text)
-    text.innerHTML = cutten_text + '...';
+    text.innerHTML = cutten_text.trim() + '...';
   }
 });
 
